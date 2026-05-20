2cf175ef0c36bd729e06ba6adcfc3f6bcd1fcc21
SamuelVillagran <samuel.villagranr@gmail.com>
2026-05-19 19:53:51 -0500

    Implementando funcionalidad de bomb

--- Code/src/domain/Level.java
+++ Code/src/domain/Level.java
@@ -93,10 +93,17 @@ public abstract class Level implements CollisionContext, Serializable {
 	}
 	
 	public void update(CollisionChecker checker) throws HardestGameException {
+		// 1. Move automatic entities (also ticks bomb counters)
 		for(AutomaticMovement am : getElementsAutomaticMovement()) {
 			am.move();
 		}
 		
+		// 2. Fire any pending bomb explosions (3x3 area, players + enemies)
+		for(Bomb bomb : getBombs()) {
+			bomb.explodeIfPending(this);
+		}
+		
+		// 3. Check direct contact between players and interactable elements
 		for(Player player : players) {
 			checker.checkContactsWithInteractable(player, this, this);
 			if(player.isDead()) {
@@ -112,6 +119,39 @@ public abstract class Level implements CollisionContext, Serializable {
 				.filter(e -> e instanceof Enemy)
 				.map(e -> (Enemy) e)
 				.toList();
+	}
+
+	/** Returns all bombs currently in the level. */
+	private List<Bomb> getBombs() {
+		return elements.values().stream()
+				.filter(e -> e instanceof Bomb)
+				.map(e -> (Bomb) e)
+				.toList();
+	}
+
+	/**
+	 * Returns all Damageable entities (players and enemies) whose bounding box
+	 * overlaps the given axis-aligned rectangle.
+	 *
+	 * @param x      left edge of the area in pixels
+	 * @param y      top  edge of the area in pixels
+	 * @param width  width  of the area in pixels
+	 * @param height height of the area in pixels
+	 */
+	public List<Damageable> getDamageablesInArea(int x, int y, int width, int height) {
+		return elements.values().stream()
+				.filter(e -> e instanceof Damageable)
+				.filter(e -> overlapsArea((Element) e, x, y, width, height))
+				.map(e -> (Damageable) e)
+				.toList();
+	}
+
+	/** AABB check: does element e overlap the rectangle (x, y, w, h)? */
+	private boolean overlapsArea(Element e, int x, int y, int width, int height) {
+		return e.getPosX() < x + width
+			&& e.getPosX() + ((HitBox) e).getWidth()  > x
+			&& e.getPosY() < y + height
+			&& e.getPosY() + ((HitBox) e).getHeight() > y;
 	}
 
 	public boolean playerHasAllCoins(Player player) {
--- Code/src/domain/Player.java
+++ Code/src/domain/Player.java
@@ -2,7 +2,10 @@ package domain;
 
 import java.io.Serializable;
 
-public abstract class Player extends Entity implements HitBox, Movable, Serializable {
+/**
+ * @implNote takeDamage() added with Claude Sonnet 4.6
+ */
+public abstract class Player extends Entity implements HitBox, Movable, Damageable, Serializable {
 
 	private int collectedCoins;
 	private int deaths;
@@ -162,6 +165,15 @@ public abstract class Player extends Entity implements HitBox, Movable, Serializ
 
 	public void destroy() {
 		setState(new DeadState(this));
+	}
+
+	/**
+	 * Receive damage from a bomb (direct contact or explosion area).
+	 * Delegates to destroy() which triggers the dead state and respawn.
+	 */
+	@Override
+	public void takeDamage(Level level) {
+		destroy();
 	}
 
 	public void addLife() {
